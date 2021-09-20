97e2498a54294c5a7e3061e98f01d969f5804f0a
LaJBel Ms <daniel12onii@gmail.com>
2021-09-20 23:08:24 +0000

    is

--- game.js
+++ game.js
@@ -93,8 +93,9 @@ scene("game", (tmode) => {
     add([ sprite("background") ]);
 
     for (let i = 0; i < eggsForSpwan; i++) {
+        const e = choose(eggs);
         let egg = add([
-            sprite(choose(eggs)),
+            sprite(e),
             pos(rand(width()), rand(height())),
             scale(7),
             origin("center"),
@@ -103,6 +104,7 @@ scene("game", (tmode) => {
             layer("game"),
             i !== 0 ? color(rgb(255, 255, 255)) : color(rgb(255, 0, 255)),
             "egg",
+            e,
             {
                 dragged: false
             }
@@ -141,34 +143,36 @@ scene("game", (tmode) => {
             ngUnlockMedal(63941);
             music.stop();
 
-            if (purpleEgg.sprite == "clasic") ngUnlockMedal(65362);
-            else if (purpleEgg.sprite == "alien") ngUnlockMedal(65358);
-            else if (purpleEgg.sprite == "angry") ngUnlockMedal(65359);
-            else if (purpleEgg.sprite == "ahegao") ngUnlockMedal(65357);
-            else if (purpleEgg.sprite == "boyfriend") ngUnlockMedal(65360);
-            else if (purpleEgg.sprite == "chapo") ngUnlockMedal(65361);
-            else if (purpleEgg.sprite == "flushed") ngUnlockMedal(65364);
-            else if (purpleEgg.sprite == "creeper") ngUnlockMedal(65363);
-            else if (purpleEgg.sprite == "furry") ngUnlockMedal(65365);
-            else if (purpleEgg.sprite == "joy") ngUnlockMedal(65366);
-            else if (purpleEgg.sprite == "madness") ngUnlockMedal(65367);
-            else if (purpleEgg.sprite == "nerd") ngUnlockMedal(65368);
-            else if (purpleEgg.sprite == "omni") ngUnlockMedal(65369);
-            else if (purpleEgg.sprite == "pacman") ngUnlockMedal(65370);
-            else if (purpleEgg.sprite == "papa") ngUnlockMedal(65371);
-            else if (purpleEgg.sprite == "roblox") ngUnlockMedal(65372);
-            else if (purpleEgg.sprite == "sad") ngUnlockMedal(65373);
-            else if (purpleEgg.sprite == "slick") ngUnlockMedal(65383);
-            else if (purpleEgg.sprite == "steve") ngUnlockMedal(65374);
-            else if (purpleEgg.sprite == "stupid") ngUnlockMedal(65375);
-            else if (purpleEgg.sprite == "sus") ngUnlockMedal(65376);
-            else if (purpleEgg.sprite == "woman") ngUnlockMedal(65380);
-            else if (purpleEgg.sprite == "xd") ngUnlockMedal(65381);
-            else if (purpleEgg.sprite == "stepford") ngUnlockMedal(65382);
-            else if (purpleEgg.sprite == "synj") ngUnlockMedal(65377);
-            else if (purpleEgg.sprite == "think") ngUnlockMedal(65378);
-            else if (purpleEgg.sprite == "mark") ngUnlockMedal(65384);
-            else if (purpleEgg.sprite == "uwu") ngUnlockMedal(65379);
+            console.log(purpleEgg);
+
+            if (purpleEgg.is("clasic")) ngUnlockMedal(65362);
+            else if (purpleEgg.is("alien")) ngUnlockMedal(65358);
+            else if (purpleEgg.is("angry")) ngUnlockMedal(65359);
+            else if (purpleEgg.is("ahegao")) ngUnlockMedal(65357);
+            else if (purpleEgg.is("boyfriend")) ngUnlockMedal(65360);
+            else if (purpleEgg.is("chapo")) ngUnlockMedal(65361);
+            else if (purpleEgg.is("flushed")) ngUnlockMedal(65364);
+            else if (purpleEgg.is("creeper")) ngUnlockMedal(65363);
+            else if (purpleEgg.is("furry")) ngUnlockMedal(65365);
+            else if (purpleEgg.is("joy")) ngUnlockMedal(65366);
+            else if (purpleEgg.is("madness")) ngUnlockMedal(65367);
+            else if (purpleEgg.is("nerd")) ngUnlockMedal(65368);
+            else if (purpleEgg.is("omni")) ngUnlockMedal(65369);
+            else if (purpleEgg.is("pacman")) ngUnlockMedal(65370);
+            else if (purpleEgg.is("papa")) ngUnlockMedal(65371);
+            else if (purpleEgg.is("roblox")) ngUnlockMedal(65372);
+            else if (purpleEgg.is("sad")) ngUnlockMedal(65373);
+            else if (purpleEgg.is("slick")) ngUnlockMedal(65383);
+            else if (purpleEgg.is("steve")) ngUnlockMedal(65374);
+            else if (purpleEgg.is("stupid")) ngUnlockMedal(65375);
+            else if (purpleEgg.is("sus")) ngUnlockMedal(65376);
+            else if (purpleEgg.is("woman")) ngUnlockMedal(65380);
+            else if (purpleEgg.is("xd")) ngUnlockMedal(65381);
+            else if (purpleEgg.is("stepford")) ngUnlockMedal(65382);
+            else if (purpleEgg.is("synj")) ngUnlockMedal(65377);
+            else if (purpleEgg.is("think")) ngUnlockMedal(65378);
+            else if (purpleEgg.is("mark")) ngUnlockMedal(65384);
+            else if (purpleEgg.is("uwu")) ngUnlockMedal(65379);
         };
 
         if (win == true && !purpleEgg.dragged) {
